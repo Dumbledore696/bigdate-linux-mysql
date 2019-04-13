570a1e615ea268f3b0b3fc71ee7d0288939a2c89
Dumbledore696 <1362371931@qq.com>
2019-04-13 19:35:46 +0800

    修改test.py

--- test.py
+++ test.py
@@ -4,10 +4,3 @@ disk_total = os.popen("df -h | sed '1d' | awk '{sum += $2};END {print sum}'").re
 s=time.asctime()
 print(type(s))
 print(type(disk_total))
-# h='host1'
-# s.add(h)
-# if "host1"in s:
-#     print ("yes")
-#     print(type(h))
-# else:
-#     print("No")
