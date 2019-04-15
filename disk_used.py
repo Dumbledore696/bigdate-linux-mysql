b34ac1278969ca69312919fc223fdea16512b943
Dumbledore696 <1362371931@qq.com>
2019-04-15 21:11:33 +0800

    修改磁盘信息读取算法

--- disk_used.py
+++ disk_used.py
@@ -1,5 +1,23 @@
-import os
-
+# encoding: utf-8
+#root下才能出结果
+import commands
+import  psutil
 def get_disk_used():
-    f=os.popen("df -h | sed '1d' | awk '{sum += $5};END {print sum}'")
-    return f.read()
+
+    mount = commands.getoutput('mount -v')
+    lines = mount.split('\n')
+    points = map(lambda line: line.split()[2], lines)
+    total=0
+    used=0
+    for i in points:
+        total+=float(psutil.disk_usage(i).total)
+        used+=float(psutil.disk_usage(i).used)
+
+    total=total/1024/1024/1024
+    used=used/1024/1024/1024
+    tot=('%.2f')%total
+    percent=("%.2f")%((used/total)*100)
+    return (tot,percent)
+# if __name__ == '__main__':
+#     print get_disk_used()[1]
+#     print str(get_disk_used()[0])
