77d9b04cae2bcf8de788ecc440ad68866888fb48
Dumbledore696 <1362371931@qq.com>
2019-04-16 21:02:29 +0800

    修改内存获取代码

--- mem_used.py
+++ mem_used.py
@@ -1,5 +1,10 @@
 import os
 def get_mem_used():
-    # f = os.popen("free -m |grep Mem |awk '{print $2,$3}'")
-    f=os.popen("cat /proc/meminfo | sed '2d' | awk 'NR==1,NR==2 {print $2}'")
-    return f.read()
+    f=os.popen("free -m | sed '1d' | awk 'NR==1 {print $2,$3}' ")
+    l=f.read().split()
+    used=l[1]
+    total=l[0]
+    used=float(used)
+    total=float(total)
+    return (total,used)
+
