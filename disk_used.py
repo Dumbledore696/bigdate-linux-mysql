c3459f994b7c2ae8a44749c196aa3577e14d87ad
Dumbledore696 <1362371931@qq.com>
2019-05-19 13:12:31 +0800

    修复利用率为0

--- disk_used.py
+++ disk_used.py
@@ -1,35 +1,37 @@
-#encoding: utf-8
-#root下才能出结果
-import commands
-import os
-import  psutil
-def get_disk_used():
-	total=psutil.disk_usage('/').total/1024/1024/1024
-    used=psutil.disk_usage('/').used/1024/1024/1024
-    sum= ('%.2f') % total
-    percent=("%.2f")%((used/total)*100)
-    #f = os.popen("fdisk -l").readlines()
-    #sum = 0
-    # print f
-    #for line in f:
-    #    if (line.startswith('Disk /dev/sda:')) or (line.startswith('Disk /dev/sdb:')):
-    #        sum += float(line.split()[2])#只能用float强制转型，因为字符串中有小数点
-    #mount = commands.getoutput('mount -v')
-    #lines = mount.split('\n')
-    #map()是 Python 内置的高阶函数，它接收一个函数 f 和一个 list，并通过把函数 f 依次作用在 list 的每个元素上，得到一个新的 list 并返回
-    #points = map(lambda line: line.split()[2], lines)
-    #total=0
-    #used=0
-    #for i in points:
-    #    total+=float(psutil.disk_usage(i).total)
-    #    used+=float(psutil.disk_usage(i).used)
+# encoding: utf-8
+from __future__ import division
+import sys
+#sys.path.append('/./home/njit/PycharmProjects/Host/venv/lib/python2.7/site-packages')
+sys.path.append('/./home/Host/venv/lib/python2.7/site-packages')
 
-    #total=total/1024/1024/1024
-    #used=used/1024/1024/1024
-    #tot=('%.2f')%total
-    #percent=("%.2f")%((used/total)*100)
-    return (sum,percent)
-# if __name__ == '__main__':
-#     print get_disk_used()[1]
-#     print str(get_disk_used()[0])
-# print psutil.disk_usage('/')
+import  os
+import commands
+import pymysql,psutil
+import time, conf
+import socket
+def get_disk_used():
+#now = time.strftime('%Y.%m.%d %H:%M:%S', time.localtime(time.time()))
+#a=psutil.disk_usage('/')
+#print a
+	total=psutil.disk_usage('/').total/1024/1024/1024
+	used=psutil.disk_usage('/').used/1024/1024/1024
+	sum2=('%.2f') % total
+	percent=("%.2f")%((used/total)*100)
+	return (sum2,percent)
+#if __name__ == '__main__':
+#	get_disk_used() 
+#mount=commasmount = commands.getoutput('mount -v')  
+#lines = mount.split('\n')  
+#points = map(lambda line: line.split()[2], lines)  
+#total=0  
+#used=0  
+#for i in points:  
+#	total+=float(psutil.disk_usage(i).total)  
+#	used+=float(psutil.disk_usage(i).used)  
+#total=total/1024/1024/1024  
+#used=used/1024/1024/1024  
+#tot=('%.2f')%total  
+#percent=("%.2f")%((used/total)*100)  
+#print percent
+#print tot
+#print used
